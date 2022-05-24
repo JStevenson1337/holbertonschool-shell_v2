7a24b66e887162aa5b5bf4ff8b1d9216c25d51b8
jeremy stevenson <jeremy.m.stevenson@gmail.com>
2022-05-23 19:15:34 -0500

    add: getpid

---
+++ hsh.h
@@ -0,0 +1,7 @@
+#ifndef __HSH_H__
+#define __HSH_H__
+
+#include <stdio.h>
+#include <unistd.h>
+
+#endif /* __HSH_H__ */
---
+++ main.c
@@ -0,0 +1,17 @@
+#include "hsh.h"
+/**
+ * main - super simple shell
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+
+
+	if (!argc)
+		return (0);
+
+	return (0);
+}
